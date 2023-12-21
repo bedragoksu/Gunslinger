a285d8c2571b7aafb185e219a0da426666569046
senacakirli <senacakirli@gmail.com>
2023-12-21 23:55:00 +0300

    ui updates

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private Card card;
+    [SerializeField]
+    private TextMeshPro nameText;
+    private Image pictureImage;
+    private Image symbolImage;
+    private Text numberText;
+
+    void Start()
+    {
+        nameText.text = card.name;
+
+        pictureImage.sprite = card.picture;
+        symbolImage.sprite = card.symbol;
+
+        numberText.text = card.number.ToString();
+    }
+}
