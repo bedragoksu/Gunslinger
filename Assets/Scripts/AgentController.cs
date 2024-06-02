b53d692a1de4762891ac646b9ce6efa5af83e167
b2200356102 <bedragoksu@gmail.com>
2024-06-02 21:13:07 +0300

    agent player info

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+
+    [SerializeField] private GameManager _gameManager;
+
+    private class InfoPlayer
+    {
+        public string Name;
+        public int CurrentBulletNumber;
+        public bool IsAlive;
+        public int CardNumber;
+        public PlayerModel.TypeOfPlayer Role;
+
+        public InfoPlayer(string Name, int CurrentBulletNumber, int CardNumber, PlayerModel.TypeOfPlayer Role = PlayerModel.TypeOfPlayer.Bos)
+        {
+            this.Name = Name;
+            this.CurrentBulletNumber = CurrentBulletNumber;
+            //this.IsAlive = IsAlive;
+            this.CardNumber = CardNumber;
+            this.Role = Role;
+        }
+    }
+
+    // currentbulletpoint, IsAlive, cardnum, varsa rol, name,acik kartlar
+    public void AgentDecideToPlay(GameObject AgentPlayer)
+    {
+        PlayerModel AgentPlayerModel = AgentPlayer.GetComponent<PlayerModel>();
+        Dictionary<int, InfoPlayer> PlayerInfos = CreatePlayerInfos(AgentPlayerModel);
+
+        // decide tree
+
+    }
+
+
+
+    private Dictionary<int, InfoPlayer> CreatePlayerInfos(PlayerModel AgentPlayerModel)
+    {
+        Dictionary<int, InfoPlayer> PlayerInfos = new Dictionary<int, InfoPlayer>();
+        foreach (GameObject player in _gameManager._turns)
+        {
+            PlayerModel playerModel = player.GetComponent<PlayerModel>();
+            if (playerModel.PlayerID != AgentPlayerModel.PlayerID && playerModel.IsAlive)
+            {
+                InfoPlayer info = new (playerModel.PlayerName, playerModel.CurrentBulletPoint, playerModel.openHand.Count,
+                                                    (playerModel.PlayerRole == PlayerModel.TypeOfPlayer.Sheriff) ? PlayerModel.TypeOfPlayer.Sheriff : PlayerModel.TypeOfPlayer.Bos);
+                PlayerInfos.Add(playerModel.PlayerID, info);
+            }
+        }
+
+        return PlayerInfos;
+    }
+
+    
+}
